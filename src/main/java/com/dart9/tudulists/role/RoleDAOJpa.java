c784eb94907c4ac431ab13598bea9e483dc98607
DartDart72 <david.regnault.cherbourg@gmail.com>
2020-11-21 18:59:21 +0100

    Mise à jour de l'accès à la base de donnée

--- src/main/java/com/dart9/tudulists/role/RoleDAOJpa.java
+++ src/main/java/com/dart9/tudulists/role/RoleDAOJpa.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository ("roleDAO")
-@Transactional
 @Scope("singleton")
 public class RoleDAOJpa implements RoleDAO {
 
